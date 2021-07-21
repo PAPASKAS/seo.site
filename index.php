0fdb83d876e3f5f5a7f81f9c82b589d443da6940
Pavel <Papaskaaaa@gmail.com>
2021-07-22 00:02:40 +0300

    Info for a sercives / link pyromids / wide sope
    Added this blocks. Last two block without mobile version

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require_once "blocks/head.html";
 require_once "blocks/header.php";
 require_once "blocks/info for a services.html";
+require_once "blocks/Link pyromids info social media.html";
+require_once "blocks/Wide scope deeper understanding.html";
 
 
 
